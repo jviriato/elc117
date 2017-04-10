31587c84a038b992f9ef25f6a9df69a9abf63a49
jviriato <jvviriato@inf.ufsm.br>
2017-04-10 20:05:58 -0300

    add t3parte3

---
+++ t3/t3parte3.pl
@@ -0,0 +1,6 @@
+% Triângulos
+% Uma série de diagramas com triângulos é construída usando palitos de fósforo, como mostrado na figura abaixo.
+% Questão 1.
+% Quantos palitos são necessários para construir o diagrama de número 5?
+
+palitos(N, R) :- R is N*2+1.
